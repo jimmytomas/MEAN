fe177a743dbfc8906c154a7edce64930a690a685
Jaime Rodriguez <jaimeomar.rodriguez@gmail.com>
2017-11-12 16:55:23 -0600

    Add song functionality

--- controllers/song.js
+++ controllers/song.js
@@ -20,6 +20,43 @@ function getSong(req, res){
     });
 }
 
+function getSongs(req, res){
+    var albumId = req.params.id;
+
+    if(!albumId){
+        var find = Song.find({}).sort('number');
+    }else{
+        var find = Song.findById(albumId).sort('number');
+    }
+
+    find.populate({ 
+        path: 'album',
+        select: 'title',
+        populate: {
+            path: 'artist',
+            select: 'name',
+            model: 'Artist'
+        }
+    }).exec((err, songs) => {
+        if(!err){
+            if(songs){
+                res.status(200).send({ songs });
+            }
+        }
+    })
+}
+
+function updateSong(req, res){
+    var songId = req.params.id;
+    var update = req.body;
+
+    Song.findByIdAndUpdate( songId, update, (err, songUpdated) => {
+        if(!err && songUpdated){
+            res.status(200).send({ song: songUpdated });
+        }
+    });
+}
+
 function saveSong(req, res){
     var song = new Song();
     var params = req.body;
@@ -37,10 +74,62 @@ function saveSong(req, res){
             }
         }
     });
+}
 
+function deleteSong(req, res){
+    var songId = req.params.id;
+
+    Song.findByIdAndRemove(songId, (err, songDeleted) => {
+        if(!err && songDeleted){
+            res.status(200).send({ song: songDeleted });
+        }
+    })
+}
+
+function uploadFile(req, res){
+    var songId = req.params.id;
+    var file_name = 'No uploaded...';
+
+    if(req.files){
+        var file_path = req.files.file.path;
+        file_name = file_path.split('\\')[2];
+        var file_ext = file_name.split('\.')[1];
+
+        if(file_ext.toLowerCase() == 'mp3'){
+            Song.findByIdAndUpdate(songId, {file: file_name}, (err, songUpdated) => {
+                if(err){
+                    res.status(500).send({ message: 'Error while updating file in DB.'});
+                }else{
+                    res.status(200).send({ song: songUpdated });
+                }
+            });
+        }else{
+            res.status(500).send({ message: 'Extension incorrect.'});
+        }
+        console.log(file_name+" "+file_ext);
+    }else{
+        req.status(200).send({ message: 'File was not uploaded.'});
+    }
+}
+
+function getSongFile(req, res){
+    var songFile = req.params.songFile;
+    var pathFile = './uploads/songs/'+songFile;
+    fs.exists(pathFile, (exist) => {
+        if(exist){
+            res.sendFile(path.resolve(pathFile));
+        }else{
+            res.status(200).send({ message: 'File dont exist.'});
+        }
+    });
 }
 
 module.exports = {
     getSong,
-    saveSong
+    saveSong,
+    getSongs,
+    updateSong,
+    deleteSong,
+    uploadFile,
+    getSongFile
 }
--- routes/song.js
+++ routes/song.js
@@ -5,7 +5,15 @@ var SongController = require('../controllers/song');
 var api = express.Router();
 var md_auth = require('../middlewares/authenticated');
 
+var multipart = require('connect-multiparty');
+var md_upload = multipart({ uploadDir: './uploads/songs' });
+
 api.get('/song/:id', md_auth.ensureAuth, SongController.getSong);
+api.get('/songs/:id?', md_auth.ensureAuth, SongController.getSongs);
 api.post('/song', md_auth.ensureAuth, SongController.saveSong);
+api.put('/song/:id', md_auth.ensureAuth, SongController.updateSong);
+api.delete('/song/:id', md_auth.ensureAuth, SongController.deleteSong);
+api.post('/upload-file-song/:id', [md_auth.ensureAuth, md_upload], SongController.uploadFile);
+api.get('/get-song-file/:songFile', md_auth.ensureAuth, SongController.getSongFile);
 
 module.exports = api;
